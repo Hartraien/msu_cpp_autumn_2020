f7219aa7a5c6d2fb7459a845ec05bc040cfdf84f
Hartraien <hartra@yandex.ru>
2020-10-21 21:05:47 +0300

    small text fix

--- 02/test/test.cpp
+++ 02/test/test.cpp
@@ -5,22 +5,27 @@
 int main(int argc, char **argv)
 {
     std::unique_ptr<TokenParserTester> tokenParserTester = std::make_unique<TokenParserTester>();
-    std::cout<< "This test will test methods of TokenParser in this order:"<<std::endl;
-    std::cout<< "TokenParser::setNumberTokenCallback"<<std::endl;
-    std::cout<< "TokenParser::setStringTokenCallback"<<std::endl;
-    std::cout<< "TokenParser::setStartCallback"<<std::endl;
-    std::cout<< "TokenParser::setFinalCallback"<<std::endl;
-    std::cout<< "TokenParser::parse"<<std::endl;
-    std::cout<< "TokenParser::parse will be test with both default and custom callback functions"<<std::endl;
-    std::cout << std::endl << std::endl;
+    std::cout << "This test will test methods of TokenParser in this order:" << std::endl;
+    std::cout << "TokenParser::setNumberTokenCallback" << std::endl;
+    std::cout << "TokenParser::setStringTokenCallback" << std::endl;
+    std::cout << "TokenParser::setStartCallback" << std::endl;
+    std::cout << "TokenParser::setFinalCallback" << std::endl;
+    std::cout << "TokenParser::parse" << std::endl;
+    std::cout << "TokenParser::parse will be tested with both default and custom callback functions (in different combinations)" << std::endl;
+    std::cout << std::endl
+              << std::endl;
     tokenParserTester->test_setNumberTokenCallback();
-    std::cout << std::endl << std::endl;
+    std::cout << std::endl
+              << std::endl;
     tokenParserTester->test_setStringTokenCallback();
-    std::cout << std::endl << std::endl;
+    std::cout << std::endl
+              << std::endl;
     tokenParserTester->test_setStartCallback();
-    std::cout << std::endl << std::endl;
+    std::cout << std::endl
+              << std::endl;
     tokenParserTester->test_setFinalCallback();
-    std::cout << std::endl << std::endl;
+    std::cout << std::endl
+              << std::endl;
     tokenParserTester->test_parse();
     std::cout << std::endl
               << "!!!!  All tests were passed successfully  !!!!"
