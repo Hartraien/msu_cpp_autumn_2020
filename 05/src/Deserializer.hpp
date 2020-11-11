30e33ee966eb26508966354300699012d8d9e140
Hartraien <hartra@yandex.ru>
2020-11-11 18:28:54 +0300

    Created hpp and cpp files and copied unedited make

---
+++ 05/src/Deserializer.hpp
@@ -0,0 +1,10 @@
+#ifndef __DESERIALIZER_HPP__
+#define __DESERIALIZER_HPP__
+
+
+class Deserializer
+{
+
+};
+
+#endif // __DESERIALIZER_HPP__
---
+++ 05/src/Serializer.hpp
@@ -0,0 +1,9 @@
+#ifndef __SERIALIZER_HPP__
+#define __SERIALIZER_HPP__
+
+class Serializer
+{
+
+};
+
+#endif // __SERIALIZER_HPP__
