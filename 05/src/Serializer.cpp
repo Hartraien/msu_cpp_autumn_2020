98a98396e55483595daf513faebaac83c2606d4e
Hartraien <hartra@yandex.ru>
2020-11-12 17:47:28 +0300

    Serializer implmeneted

---
+++ 05/src/Serializer.cpp
@@ -0,0 +1,10 @@
+#include "Serializer.hpp"
+
+Serializer::Serializer(std::ostream &out) : out_(out)
+{
+}
+
+std::string Serializer::process()
+{
+    return "";
+}
--- 05/src/Serializer.hpp
+++ 05/src/Serializer.hpp
@@ -1,9 +1,85 @@
 #ifndef __SERIALIZER_HPP__
 #define __SERIALIZER_HPP__
+#include <iostream>
+#include <cstdint>
+#include <typeinfo>
+#include <string>
+
+enum class Error
+{
+    NoError,
+    CorruptedArchive
+};
 
 class Serializer
 {
+public:
+    explicit Serializer(std::ostream &out);
+    template <class T>
+    Error save(T &object);
+    template <class... ArgsT>
+    Error operator()(ArgsT... args);
 
+private:
+    std::string process();
+    template <class T, class... ArgsT>
+    std::string process(T arg1, ArgsT... args);
+
+private:
+    std::ostream &out_;
+    inline static const std::string error_string = "!";
+    inline static const std::string Separator = " ";
 };
+
+template <class T>
+Error Serializer::save(T &object)
+{
+    return object.serialize(*this);
+}
+
+template <class... ArgsT>
+Error Serializer::operator()(ArgsT... args)
+{
+    std::string res = this->process(args...);
+    if (res.find(this->error_string) == std::string::npos)
+    {
+        if (res.length() > 0)
+        {
+            res = res.substr(0, res.length() - 1);
+        }
+        this->out_ << res;
+        return Error::NoError;
+    }
+    else
+    {
+        return Error::CorruptedArchive;
+    }
+}
+
+template <class T, class... ArgsT>
+std::string Serializer::process(T arg1, ArgsT... args)
+{
+    std::string res = "";
+    if (typeid(arg1) == typeid(bool))
+    {
+        if (arg1 == true)
+        {
+            res = "true" + this->Separator;
+        }
+        else
+        {
+            res = "false" + this->Separator;
+        }
+    }
+    else if (typeid(arg1) == typeid(uint64_t))
+    {
+        res = std::to_string(arg1) + this->Separator;
+    }
+    else
+    {
+        return "!";
+    }
+    return res + this->process(args...);
+}
 
 #endif // __SERIALIZER_HPP__
